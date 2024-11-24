e66fec612486df5c469b797fa7b2e1d8df3ec208
HeYanbo <bozaigao.a@gmail.com>
2024-11-24 16:47:04 +0800

    init

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+const { config: harmonyConfig } = require('@react-native-oh/react-native-harmony-cli')
+
+/**
+ * @type {import("@react-native-community/cli-types").Config}
+ */
+const config = {
+  commands: harmonyConfig.commands,
+}
+
+module.exports = config
